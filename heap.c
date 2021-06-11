d0d415649ab8db3f004b328e98ea53d76fbbf85d
antran@caltech.edu <antran@caltech.edu>
2021-06-11 02:12:52 +0000

    Added some abstractions

--- heap.c
+++ heap.c
@@ -2,6 +2,13 @@
 
 #include <stdlib.h>
 
+typedef struct heap {
+    /** Generic array of pointers. */
+    void **ptr;
+    /** How many pointers there are currently in the array. */
+    int32_t count;
+} heap_t;
+
 heap_t *heap_init() {
     // The heap array is initially allocated to hold zero elements.
     heap_t *heap = malloc(sizeof(heap_t));
@@ -10,8 +17,20 @@ heap_t *heap_init() {
     return heap;
 }
 
-void free_heap(heap_t *heap) {
-    for (size_t i = 0; i < heap->count; i++) {
+int32_t heap_add(heap_t *heap, void *ptr) {
+    heap->ptr = realloc(heap->ptr, (heap->count + 1) * sizeof(void *));
+    heap->ptr[heap->count] = ptr;
+    int32_t temp = heap->count;
+    heap->count += 1;
+    return temp;
+}
+
+void *heap_get(heap_t *heap, int32_t ref) {
+    return heap->ptr[ref];
+}
+
+void heap_free(heap_t *heap) {
+    for (int32_t i = 0; i < heap->count; i++) {
         free(heap->ptr[i]);
     }
     free(heap->ptr);
--- heap.h
+++ heap.h
@@ -1,17 +1,34 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+#include <inttypes.h>
+
 /**
  * Represents the array of heap-allocated pointers.
  */
-typedef struct {
-    /** Generic array of pointers. */
-    void **ptr;
-    /** How many pointers there are currently in the array. */
-    size_t count;
-} heap_t;
+typedef struct heap heap_t;
 
 /**
  * Initializes a heap. The capacity of this heap is initially zero.
  */
 heap_t *heap_init();
+
+/**
+ * Add a pointer to the heap and get a reference. For simplification, the
+ * reference is an index into a generic heap-allocated array.
+ *
+ * @param ptr Pointer to add to the heap.
+ * @returns A "reference" to the pointer.
+ */
+int32_t heap_add(heap_t *heap, void *ptr);
+
+/**
+ * Retrieve a pointer from the heap.
+ *
+ * @param ref A "reference".
+ * @returns A pointer from the heap.
+ */
+void *heap_get(heap_t *heap, int32_t ref);
 
 /**
  * Frees elements of the heap-allocated pointers. This does not properly free
@@ -20,3 +37,5 @@ heap_t *heap_init();
  * @param heap array of heap-allocated pointers
  */
 void free_heap(heap_t *heap);
+
+#endif
