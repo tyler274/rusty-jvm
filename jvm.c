5d21fd1642e3bdcbb50ae1cdb27e66bb377647b8
tport@caltech.edu <tport@caltech.edu>
2021-10-10 11:23:29 +0000

    tasks 3 complete and tests pass

--- jvm.c
+++ jvm.c
@@ -20,13 +20,21 @@ const char MAIN_METHOD[] = "main";
  */
 const char MAIN_DESCRIPTOR[] = "([Ljava/lang/String;)V";
 
-const int32_t null_four_bytes = 0x0000;
-const size_t two_operand_offset = 2;
+const int32_t NULL_FOUR_BYTES = 0x0000;
+const size_t TWO_OPERAND_OFFSET = 2;
+const int32_t NEGATIVE_ONE = -1;
+const int32_t ZERO = 0;
+const int32_t ONE = 1;
+const int32_t TWO = 2;
+const int32_t THREE = 3;
+const int32_t FOUR = 4;
+const int32_t FIVE = 5;
 
 /**
  * Represents the return value of a Java method: either void or an int or a reference.
- * For simplification, we represent a reference as an index into a heap-allocated array.
- * (In a real JVM, methods could also return object references or other primitives.)
+ * For simplification, we represent a reference as an index into a heap-allocated
+ * array. (In a real JVM, methods could also return object references or other
+ * primitives.)
  */
 typedef struct {
     /** Whether this returned value is an int */
@@ -37,8 +45,12 @@ typedef struct {
 
 // simple stack implementation
 typedef struct {
+    // the maximum size of the stack
     size_t size;
+    // the index of the current top of the stack
     size_t top;
+    // the callee must allocate this on the heap for now, and free it using the associated
+    // function.
     int32_t *contents;
 } stack_t;
 
@@ -81,7 +93,7 @@ int32_t stack_push(stack_t *stack, int32_t value) {
 int32_t stack_pop(stack_t *stack, int32_t *value) {
     if (stack->top > 0) {
         *value = stack->contents[stack->top];
-        stack->contents[stack->top] = null_four_bytes;
+        stack->contents[stack->top] = NULL_FOUR_BYTES;
         stack->top--;
         // return 1 if something is popped off
         return 1;
@@ -94,67 +106,156 @@ int32_t stack_pop(stack_t *stack, int32_t *value) {
     }
 }
 
+__always_inline void iconst_helper(jvm_instruction_t opcode, stack_t *stack,
+                                   size_t *program_counter) {
+    // this instruction always increments the counter by 1
+    (*program_counter)++;
+    // we can take advantage of the opcode numbers to
+    int32_t con = ((int32_t) opcode) - THREE;
+    int32_t push_result = 0;
+    push_result = stack_push(stack, con);
+    assert(push_result == 1);
+}
+
+__always_inline void bipush_helper(stack_t *stack, size_t *program_counter,
+                                   method_t *method) {
+    // increment program counter by a total of two, one for the bipush instruction
+    // itself, and another for the operand after pushing it to the stack.
+    (*program_counter)++;
+    // pushes the operand onto the stack.
+    // remember the second program_counter increment here
+    int32_t value = 0;
+    fprintf(stderr, "%08x", method->code.code[(*program_counter)]);
+    value = (signed int) ((signed char) method->code.code[(*program_counter)++]);
+    int32_t push_result = stack_push(stack, value);
+    assert(push_result == 1);
+}
+
+__always_inline void sipush_helper(stack_t *stack, size_t *program_counter,
+                                   method_t *method) {
+    // increment program counter by a total of three, one for the sipush instruction
+    // itself, and two for the two operands after pushing them on to the stack.
+    (*program_counter)++;
+    u1 first_operand = 0;
+    u1 second_operand = 0;
+    // remember the second program_counter increment here
+    first_operand = method->code.code[(*program_counter)++];
+    // remember the third program_counter increment here
+    second_operand = method->code.code[(*program_counter)++];
+
+    signed short result = 0;
+    result = (((signed short) first_operand << 8) | second_operand);
+    int32_t push_result = stack_push(stack, result);
+    assert(push_result == 1);
+}
+
+__always_inline void iadd_helper(stack_t *stack, size_t *program_counter) {
+    // add instruction
+    // increment the program counter by one, as add doesn't take any operands
+    (*program_counter)++;
+    int32_t first_operand = 0;
+    int32_t second_operand = 0;
+    int32_t pop_result, push_result;
+    // pop our second operand (pushed to the stack last).
+    pop_result = stack_pop(stack, &second_operand);
+    assert(pop_result == 1);
+    // pop our first operand
+    pop_result = stack_pop(stack, &first_operand);
+    assert(pop_result == 1);
+    // push the result back on to the stack.
+    push_result = stack_push(stack, first_operand + second_operand);
+    assert(push_result == 1);
+}
+
+__always_inline void return_helper(size_t *program_counter, method_t *method) {
+    // return by setting the program counter to the end of the instruction list,
+    // thus breaking the while loop
+    *program_counter = method->code.code_length;
+}
+
+__always_inline void getstatic_helper(size_t *program_counter) {
+    // increment the program counter for the `getstatic` opcode itself.
+    (*program_counter)++;
+
+    // increment it past the next two opcodes, for a total incrementation for each
+    // use of this instruction of three, per the spec.
+    (*program_counter) += TWO_OPERAND_OFFSET;
+}
+
+__always_inline void invokevirtual_helper(stack_t *stack, size_t *program_counter) {
+    // invokevirtual b1 b2
+    // Pops and prints the top value of the operand stack followed by a newline
+    // character. Then, moves the program counter past b2 (i.e., increments it by
+    // three).
+    (*program_counter)++;
+
+    int32_t pop_result, value;
+    pop_result = stack_pop(stack, &value);
+    fprintf(stdout, "%d\n", value);
+    (*program_counter) += TWO_OPERAND_OFFSET;
+}
+
+// essentially a giant vtable that matches the opcode and handles running that
+// instruction's implementation
 void opcode_helper(stack_t *stack, size_t *program_counter, method_t *method,
                    int32_t *locals, class_file_t *class, heap_t *heap) {
-    switch ((jvm_instruction_t) method->code.code[*program_counter]) {
-        case i_nop: {
-            // not implemented but its a no-op anyway
+    jvm_instruction_t opcode = (jvm_instruction_t) method->code.code[*program_counter];
+    switch (opcode) {
+        case i_nop: { // not implemented but its a no-op anyway
+            break;
+        }
+        case i_iconst_m1: {
+            iconst_helper(opcode, stack, program_counter);
+            break;
+        }
+        case i_iconst_0: {
+            iconst_helper(opcode, stack, program_counter);
+            break;
+        }
+        case i_iconst_1: {
+            iconst_helper(opcode, stack, program_counter);
+            break;
+        }
+        case i_iconst_2: {
+            iconst_helper(opcode, stack, program_counter);
+            break;
+        }
+        case i_iconst_3: {
+            iconst_helper(opcode, stack, program_counter);
+            break;
+        }
+        case i_iconst_4: {
+            iconst_helper(opcode, stack, program_counter);
+            break;
+        }
+        case i_iconst_5: {
+            iconst_helper(opcode, stack, program_counter);
             break;
         }
         case i_bipush: {
-            // increment program counter by a total of two, one for the push instruction
-            // itself, and another for the operand after pushing it to the stack.
-            (*program_counter)++;
-            // remember the second increment here
-            int32_t push_result =
-                stack_push(stack, method->code.code[(*program_counter)++]);
-            assert(push_result == 1);
-
+            bipush_helper(stack, program_counter, method);
+            break;
+        }
+        case i_sipush: {
+            sipush_helper(stack, program_counter, method);
             break;
         }
         case i_iadd: {
-            // add instruction
-            // increment the program counter by one, as add doesn't take any operands
-            (*program_counter)++;
-            int32_t first_operand, second_operand, pop_result, push_result;
-            pop_result = stack_pop(stack, &second_operand);
-            assert(pop_result == 1);
-            pop_result = stack_pop(stack, &first_operand);
-            assert(pop_result == 1);
-            push_result = stack_push(stack, first_operand + second_operand);
-            assert(push_result == 1);
-
+            iadd_helper(stack, program_counter);
             break;
         }
         case i_return: {
-            // return by setting the program counter to the end of the instruction list,
-            // thus breaking the while loop
-            *program_counter = method->code.code_length;
+            return_helper(program_counter, method);
             break;
         }
 
         case i_getstatic: {
-            // increment the program counter for the `getstatic` opcode itself.
-            (*program_counter)++;
-
-            // increment it past the next two opcodes, for a total incrementation for each
-            // use of this instruction of three, per the spec.
-            (*program_counter) += two_operand_offset;
-
+            getstatic_helper(program_counter);
             break;
         }
 
         case i_invokevirtual: {
-            // invokevirtual b1 b2
-            // Pops and prints the top value of the operand stack followed by a newline
-            // character. Then, moves the program counter past b2 (i.e., increments it by
-            // three).
-            (*program_counter)++;
-
-            int32_t pop_result, value;
-            pop_result = stack_pop(stack, &value);
-            fprintf(stdout, "%d\n", value);
-            (*program_counter) += two_operand_offset;
+            invokevirtual_helper(stack, program_counter);
             break;
         }
 
